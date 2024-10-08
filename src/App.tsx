f0fe16365d01051263738563078876323b2d0b25
Evandro Costa <evandro.souzac@gmail.com>
2024-10-08 14:00:10 -0300

    create app `start-page`

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [startGame, setStartGame] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="container">
+      {!startGame ? (
+        <div className="start-screen">
+          <h1 className="start-title">Quizzical</h1>
+          <h2 className="start-description">
+            Test your knowledge with this awesome trivia game!
+          </h2>
+          <button className="start-button" onClick={() => setStartGame(true)}>
+            Start quiz
+          </button>
+        </div>
+      ) : (
+        <div>Quiz</div>
+      )}
+    </main>
   );
 }
-
-export default App;
